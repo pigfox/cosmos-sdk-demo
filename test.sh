1123c541354a71d36b195b440e28e3dc20eb7dcb
Peter Sjolin <pigfoxeria@gmail.com>
2025-01-23 19:03:18 -0800

    wip

--- test.sh
+++ test.sh
@@ -8,19 +8,20 @@ trap 'rc=$?; echo "error code $rc at $LINENO"; exit $rc' ERR
 # Define costants
 HOME_DIR=/home/$USER/.simd
 KEYRING_BACKEND=test
-CHAIN_ID=mychain
+KEY_NAME=mykey
+CHAIN_ID=chainid
 
 # Remove existing genesis.json file
 rm -rf $HOME_DIR/config/genesis.json
 
 # Generate keys
-echo "y" | simd keys add mykey --keyring-backend $KEYRING_BACKEND --home $HOME_DIR
+echo "y" | simd keys add $KEY_NAME --keyring-backend $KEYRING_BACKEND --home $HOME_DIR
 
 # Get the address of the key
-my_key_address=$(simd keys show mykey --keyring-backend $KEYRING_BACKEND --home $HOME_DIR --address)
+my_key_address=$(simd keys show $KEY_NAME --keyring-backend $KEYRING_BACKEND --home $HOME_DIR --address)
 
 # Get the validator address
-my_validator_address=$(simd keys show mykey --keyring-backend $KEYRING_BACKEND --home $HOME_DIR --bech val --address)
+my_validator_address=$(simd keys show $KEY_NAME --keyring-backend $KEYRING_BACKEND --home $HOME_DIR --bech val --address)
 
 # Validate the validator address prefix
 if [[ ! "$my_validator_address" =~ ^cosmosvaloper ]]; then
@@ -32,13 +33,13 @@ fi
 simd genesis add-genesis-account $my_key_address 10000stake #--append
 
 # Delegate stake to the validator using the dynamically retrieved validator address
-simd tx staking delegate $my_validator_address 500000stake --from mykey --home $HOME_DIR --keyring-backend $KEYRING_BACKEND --chain-id xzxzxz --gas auto --fees 5000stake -y
+simd tx staking delegate $my_validator_address 500000stake --from $KEY_NAME --home $HOME_DIR --keyring-backend $KEYRING_BACKEND --chain-id $CHAIN_ID --gas auto --fees 5000stake -y
 
 # Collect genesis transactions
 simd collect-gentxs --home $HOME_DIR
 
 # Initialize the chain
-simd init mychain --chain-id mychain --home $HOME_DIR
+simd init $CHAIN_ID --chain-id $CHAIN_ID --home $HOME_DIR
 
 # Start the node
 simd start --home $HOME_DIR &
@@ -50,16 +51,16 @@ sleep 5
 simd status --home $HOME_DIR
 exit
 # Delegate stake to the validator 
-simd tx staking delegate $my_validator_address 1000000000stake --from mykey --keyring-backend $KEYRING_BACKEND --home $HOME_DIR --chain-id mychain --gas auto --fees 10000stake -y
+simd tx staking delegate $my_validator_address 1000000000stake --from $KEY_NAME --keyring-backend $KEYRING_BACKEND --home $HOME_DIR --chain-id $CHAIN_ID --gas auto --fees 10000stake -y
 
 # Create a simple transaction (e.g., bank send)
-simd tx bank send $my_key_address another_address 1000000stake --from mykey --keyring-backend $KEYRING_BACKEND --home $HOME_DIR --chain-id mychain --gas auto --fees 10000stake -y 
+simd tx bank send $my_key_address another_address 1000000stake --from $KEY_NAME --keyring-backend $KEYRING_BACKEND --home $HOME_DIR --chain-id $CHAIN_ID --gas auto --fees 10000stake -y 
 
 # Query account balance
-simd query account $my_key_address --home $HOME_DIR --chain-id mychain
+simd query account $my_key_address --home $HOME_DIR --chain-id $CHAIN_ID
 
 # Query validator information
-simd query staking validator $my_validator_address --home $HOME_DIR --chain-id mychain
+simd query staking validator $my_validator_address --home $HOME_DIR --chain-id $CHAIN_ID
 
 # Stop the node
 simd stop --home $HOME_DIR
