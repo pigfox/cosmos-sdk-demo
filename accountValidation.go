b5d91e92305f396fd15a8673f53cf64f946f8ea3
Peter Sjolin <pigfoxeria@gmail.com>
2025-01-24 20:26:08 -0800

    wip

--- accountValidation.go
+++ accountValidation.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"github.com/jimlawless/whereami"
+	"os"
+	"regexp"
+)
+
 func accountValidation(account Account) {
-	//if account.
+	if account.KeyName == VALIDATOR {
+		regex := `^cosmosvaloper1[a-z0-9]{38}$`
+		matched, err := regexp.MatchString(regex, account.AccountKey.Address)
+		if err != nil {
+			fmt.Println("Error with regex:", whereami.WhereAmI(), err)
+			os.Exit(1)
+		}
+
+		if !matched {
+			fmt.Println("Error: validator address is not in the correct format", whereami.WhereAmI())
+			fmt.Println("validatorAddress:", account.AccountKey.Address, "length", len(account.AccountKey.Address))
+			os.Exit(1)
+		}
+	} else {
+		keyRegex := `^[A-Za-z0-9+/]+={0,2}$`
+		matched, err := regexp.MatchString(keyRegex, account.AccountKey.Address)
+		if err != nil {
+			fmt.Println("Error with regex:", whereami.WhereAmI(), err)
+			os.Exit(1)
+		}
+
+		if !matched {
+			fmt.Println("Invalid PubKey match.")
+			fmt.Println("Given PubKey:", account.AccountKey.Address)
+			os.Exit(1)
+		}
+	}
 }
